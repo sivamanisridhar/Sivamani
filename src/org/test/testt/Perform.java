1a45789dc6ad0ebf2da80ee87bfb43ccfe92a4cc
sivamanisridhar <sivamanisridhar07@gmal.com>
2019-04-01 12:01:30 +0530

    first commit

---
+++ src/org/test/testt/Perform.java
@@ -0,0 +1,10 @@
+package org.test.testt;
+
+public class Perform {
+public void empId(){
+	System.out.println("empId is 12345");
+}
+public void empName(){
+	System.out.println("emp Name is Sridhar");
+} 
+}
