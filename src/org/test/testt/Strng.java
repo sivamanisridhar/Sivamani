5fbcb3fd5dfcbe0084687457bf571132e798fa45
sivamanisridhar <sivamanisridhar07@gmal.com>
2019-04-01 18:54:33 +0530

    i am ready

---
+++ src/org/test/testt/Strng.java
@@ -0,0 +1,15 @@
+package org.test.testt;
+
+public class Strng {
+	
+public static void main(String[] args) {
+String dtr = "sridhar mani";
+int count = 0;
+for (int i = 0; i < dtr.length(); i++) {
+char charAt = dtr.charAt(i);
+if (charAt=='a') {
+count++;
+System.out.println(count);
+}
+}}
+}
